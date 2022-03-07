10db3d4ece37fb55919499bf03a3ae0974147a72
Heri Andria Herdiana <34875303+andryawmti@users.noreply.github.com>
2022-03-08 03:45:57 +0700

    update navbar styling (#2)

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -21,14 +21,16 @@
           >
             <li class="nav-item" role="presentation">
               <button
-                class="as-card-notification__header--link active"
+                class="as-card-notification__header--link"
+                :class="btnOneClassName"
+                @click="tabClickHandler(true)"
                 id="nav-notif-tab"
                 data-bs-toggle="pill"
                 data-bs-target="#nav-notif"
                 type="button"
                 role="tab"
                 aria-controls="nav-notif"
-                aria-selected="true"
+                :aria-selected="tabOneActive"
               >
                 Pemberitahuan
               </button>
@@ -37,13 +39,15 @@
             <li class="nav-item" role="presentation">
               <button
                 class="as-card-notification__header--link"
+                :class="btnTwoClassName"
+                @click="tabClickHandler(false)"
                 id="nav-promo-tab"
                 data-bs-toggle="pill"
                 data-bs-target="#nav-promo"
                 type="button"
                 role="tab"
                 aria-controls="nav-promo"
-                aria-selected="false"
+                :aria-selected="!tabOneActive"
               >
                 Update & Promo
               </button>
@@ -56,29 +60,30 @@
         >
           <div class="tab-content" id="pills-tabContent">
             <div
-              class="tab-pane fade show active"
+              class="tab-pane fade"
+              :class="tabOneClassName"
               id="nav-notif"
               role="tabpanel"
               aria-labelledby="nav-notif-tab"
             >
               <a href="#" class="as-card-notification__content">
-                <div class="d-flex gap-2 align-items-center as-mb--12">
+                <div class="d-flex gap-2 align-items-center as-mb-12">
                   <img
                     src="@/assets/img/status/downtime.svg"
                     class="as-card-notification__content--image"
                     loading="lazy"
                     alt="downtime"
                   />
-                  <p class="as-text--12-semibold as-color--red">Downtime</p>
+                  <p class="as-text-12-semibold as-color-red">Downtime</p>
                   <span class="as-card-notification__content--dot"></span>
-                  <p class="as-text--12 as-color-secondary-2">
+                  <p class="as-text-12 as-color-secondary-2">
                     30 Desember 2022, 17:00
                   </p>
                 </div>
-                <p class="as-text--16-bold as-mb--12">
+                <p class="as-text-16-bold as-mb-12">
                   Server akan mengalami maintanance
                 </p>
-                <p class="as-text--12">
+                <p class="as-text-12">
                   Server srv56.niagahoster.com akan mengalami maintanance pada 7
                   Oktober 2021 jam 23.30-00.30. Mohon maaf atas
                   ketidaknyamanannya. Terima kasih
@@ -88,25 +93,25 @@
                 href="#"
                 class="as-card-notification__content as-background--soft"
               >
-                <div class="d-flex gap-2 align-items-center as-mb--12">
+                <div class="d-flex gap-2 align-items-center as-mb-12">
                   <img
                     src="@/assets/img/status/product.svg"
                     class="as-card-notification__content--image"
                     loading="lazy"
                     alt="product status"
                   />
-                  <p class="as-text--12-semibold as-color--yellow">
+                  <p class="as-text-12-semibold as-color-yellow">
                     Product Status
                   </p>
                   <span class="as-card-notification__content--dot"></span>
-                  <p class="as-text--12 as-color-secondary-2">
+                  <p class="as-text-12 as-color-secondary-2">
                     30 Desember 2022, 17:00
                   </p>
                 </div>
-                <p class="as-text--16-bold as-mb--12">
+                <p class="as-text-16-bold as-mb-12">
                   Server akan mengalami maintanance
                 </p>
-                <p class="as-text--12">
+                <p class="as-text-12">
                   Server srv56.niagahoster.com akan mengalami maintanance pada 7
                   Oktober 2021 jam 23.30-00.30. Mohon maaf atas
                   ketidaknyamanannya. Terima kasih
@@ -116,25 +121,25 @@
                 href="#"
                 class="as-card-notification__content as-card-notification__content--soft"
               >
-                <div class="d-flex gap-2 align-items-center as-mb--12">
+                <div class="d-flex gap-2 align-items-center as-mb-12">
                   <img
                     src="@/assets/img/status/resource.svg"
                     class="as-card-notification__content--image"
                     loading="lazy"
                     alt="resource"
                   />
-                  <p class="as-text--12-semibold as-color--blue-dark">
+                  <p class="as-text-12-semibold as-color-blue-dark">
                     Resource
                   </p>
                   <span class="as-card-notification__content--dot"></span>
-                  <p class="as-text--12 as-color-secondary-2">
+                  <p class="as-text-12 as-color-secondary-2">
                     30 Desember 2022, 17:00
                   </p>
                 </div>
-                <p class="as-text--16-bold as-mb--12">
+                <p class="as-text-16-bold as-mb-12">
                   Server akan mengalami maintanance
                 </p>
-                <p class="as-card-notification__content--text as-text--12">
+                <p class="as-card-notification__content--text as-text-12">
                   Server srv56.niagahoster.com akan mengalami maintanance pada 7
                   Oktober 2021 jam 23.30-00.30. Mohon maaf atas
                   ketidaknyamanannya. Terima kasih jangan lupa like dan
@@ -145,23 +150,23 @@
                 href="#"
                 class="as-card-notification__content as-card-notification__content--soft"
               >
-                <div class="d-flex gap-2 align-items-center as-mb--12">
+                <div class="d-flex gap-2 align-items-center as-mb-12">
                   <img
                     src="@/assets/img/status/invoice.svg"
                     class="as-card-notification__content--image"
                     loading="lazy"
                     alt="Invoice"
                   />
-                  <p class="as-text--12-semibold as-color--red">Invoice</p>
+                  <p class="as-text-12-semibold as-color-red">Invoice</p>
                   <span class="as-card-notification__content--dot"></span>
-                  <p class="as-text--12 as-color-secondary-2">
+                  <p class="as-text-12 as-color-secondary-2">
                     30 Desember 2022, 17:00
                   </p>
                 </div>
-                <p class="as-text--16-bold as-mb--12">
+                <p class="as-text-16-bold as-mb-12">
                   Server akan mengalami maintanance
                 </p>
-                <p class="as-card-notification__content--text as-text--12">
+                <p class="as-card-notification__content--text as-text-12">
                   Server srv56.niagahoster.com akan mengalami maintanance pada 7
                   Oktober 2021 jam 23.30-00.30. Mohon maaf atas
                   ketidaknyamanannya. Terima kasih jangan lupa like dan
@@ -172,23 +177,23 @@
                 href="#"
                 class="as-card-notification__content as-card-notification__content--soft"
               >
-                <div class="d-flex gap-2 align-items-center as-mb--12">
+                <div class="d-flex gap-2 align-items-center as-mb-12">
                   <img
                     src="@/assets/img/status/promo.svg"
                     class="as-card-notification__content--image"
                     loading="lazy"
                     alt="Promo"
                   />
-                  <p class="as-text--12-semibold as-color--green">Promo</p>
+                  <p class="as-text-12-semibold as-color-green">Promo</p>
                   <span class="as-card-notification__content--dot"></span>
-                  <p class="as-text--12 as-color-secondary-2">
+                  <p class="as-text-12 as-color-secondary-2">
                     30 Desember 2022, 17:00
                   </p>
                 </div>
-                <p class="as-text--16-bold as-mb--12">
+                <p class="as-text-16-bold as-mb-12">
                   Server akan mengalami maintanance
                 </p>
-                <p class="as-card-notification__content--text as-text--12">
+                <p class="as-card-notification__content--text as-text-12">
                   Server srv56.niagahoster.com akan mengalami maintanance pada 7
                   Oktober 2021 jam 23.30-00.30. Mohon maaf atas
                   ketidaknyamanannya. Terima kasih jangan lupa like dan
@@ -198,6 +203,7 @@
             </div>
             <div
               class="tab-pane fade"
+              :class="tabTwoClassName"
               id="nav-promo"
               role="tabpanel"
               aria-labelledby="nav-promo-tab"
@@ -206,25 +212,25 @@
                 href="#"
                 class="as-card-notification__content as-background--soft"
               >
-                <div class="d-flex gap-2 align-items-center as-mb--12">
+                <div class="d-flex gap-2 align-items-center as-mb-12">
                   <img
                     src="@/assets/img/status/resource.svg"
                     class="as-card-notification__content--image"
                     loading="lazy"
                     alt="resource"
                   />
-                  <p class="as-text--12-semibold as-color--blue-dark">
+                  <p class="as-text-12-semibold as-color-blue-dark">
                     Resource
                   </p>
                   <span class="as-card-notification__content--dot"></span>
-                  <p class="as-text--12 as-color-secondary-2">
+                  <p class="as-text-12 as-color-secondary-2">
                     30 Desember 2022, 17:00
                   </p>
                 </div>
-                <p class="as-text--16-bold as-mb--12">
+                <p class="as-text-16-bold as-mb-12">
                   Server akan mengalami maintanance
                 </p>
-                <p class="as-card-notification__content--text as-text--12">
+                <p class="as-card-notification__content--text as-text-12">
                   Server srv56.niagahoster.com akan mengalami maintanance pada 7
                   Oktober 2021 jam 23.30-00.30. Mohon maaf atas
                   ketidaknyamanannya. Terima kasih jangan lupa like dan
@@ -232,23 +238,23 @@
                 </p>
               </a>
               <a href="#" class="as-card-notification__content">
-                <div class="d-flex gap-2 align-items-center as-mb--12">
+                <div class="d-flex gap-2 align-items-center as-mb-12">
                   <img
                     src="@/assets/img/status/invoice.svg"
                     class="as-card-notification__content--image"
                     loading="lazy"
                     alt="Invoice"
                   />
-                  <p class="as-text--12-semibold as-color--red">Invoice</p>
+                  <p class="as-text-12-semibold as-color--red">Invoice</p>
                   <span class="as-card-notification__content--dot"></span>
-                  <p class="as-text--12 as-color-secondary-2">
+                  <p class="as-text-12 as-color-secondary-2">
                     30 Desember 2022, 17:00
                   </p>
                 </div>
-                <p class="as-text--16-bold as-mb--12">
+                <p class="as-text-16-bold as-mb-12">
                   Server akan mengalami maintanance
                 </p>
-                <p class="as-card-notification__content--text as-text--12">
+                <p class="as-card-notification__content--text as-text-12">
                   Server srv56.niagahoster.com akan mengalami maintanance pada 7
                   Oktober 2021 jam 23.30-00.30. Mohon maaf atas
                   ketidaknyamanannya. Terima kasih jangan lupa like dan
@@ -259,23 +265,23 @@
                 href="#"
                 class="as-card-notification__content as-card-notification__content--soft"
               >
-                <div class="d-flex gap-2 align-items-center as-mb--12">
+                <div class="d-flex gap-2 align-items-center as-mb-12">
                   <img
                     src="@/assets/img/status/promo.svg"
                     class="as-card-notification__content--image"
                     loading="lazy"
                     alt="Promo"
                   />
-                  <p class="as-text--12-semibold as-color--green">Promo</p>
+                  <p class="as-text-12-semibold as-color-green">Promo</p>
                   <span class="as-card-notification__content--dot"></span>
-                  <p class="as-text--12 as-color-secondary-2">
+                  <p class="as-text-12 as-color-secondary-2">
                     30 Desember 2022, 17:00
                   </p>
                 </div>
-                <p class="as-text--16-bold as-mb--12">
+                <p class="as-text-16-bold as-mb-12">
                   Server akan mengalami maintanance
                 </p>
-                <p class="as-card-notification__content--text as-text--12">
+                <p class="as-card-notification__content--text as-text-12">
                   Server srv56.niagahoster.com akan mengalami maintanance pada 7
                   Oktober 2021 jam 23.30-00.30. Mohon maaf atas
                   ketidaknyamanannya. Terima kasih jangan lupa like dan
@@ -286,17 +292,40 @@
           </div>
         </div>
         <div class="as-card-notification__footer">
-          <a href="" class="as-text--link as-text--bold"
-            >Tandai semua sudah dibaca</a
-          >
+          <AsButtonText link>
+            Tandai semua sudah dibaca
+          </AsButtonText>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "Notification",
-};
+<script setup>
+import { computed, ref } from 'vue'
+import { AsButtonText } from '@niagahoster/components'
+
+const tabOneActive = ref(true)
+
+const tabClickHandler = (isTabOne) => {
+  tabOneActive.value = isTabOne
+}
+
+const btnOneClassName = computed(() => ({
+  'active': tabOneActive.value
+}))
+
+const btnTwoClassName = computed(() => ({
+  'active': !tabOneActive.value
+}))
+
+const tabOneClassName = computed(() => ({
+  'active': tabOneActive.value,
+  'show': tabOneActive.value, 
+}))
+
+const tabTwoClassName = computed(() => ({
+  'active': !tabOneActive.value,
+  'show': !tabOneActive.value,
+}))
 </script>
